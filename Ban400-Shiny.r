1b87323d0cfec9ea94d1eb1427c712136d6c94ca
Magnus Torvund <magnus.torvund@gmail.com>
2020-12-11 11:03:35 +0100

    Added some more error messages

--- Ban400-Shiny.r
+++ Ban400-Shiny.r
@@ -55,7 +55,7 @@ ui <- fluidPage(
                       sidebarPanel(
                         
                         numericInput("n_unique_stocks", "Amount of stocks to select a portfolio from: ", 10,
-                                     min = 1, 
+                                     min = 2, 
                                      max = max_stockselection,
                                      step = 1),
                         
@@ -246,6 +246,7 @@ server <- function(input, output, session) {
     if (!isTruthy(input$n_unique_stocks)){
       showNotification(id = "noinput1", "Please select a number of stocks", type = "error")
     }
+    
     else if (input$n_unique_stocks > 50 & input$n_unique_stocks <= max_stockselection){
       showNotification(id = "above50", "You have selected more than 50 stocks, expect 1 minute of loading time", type = "warning")
     }
@@ -258,6 +259,7 @@ server <- function(input, output, session) {
     }
   })
   
+  # Error 1 - Check if risk free rate is specified
   observe({
     if (!isTruthy(input$rfrate)){
       showNotification(id = "noRfRate", "Please specify risk free rate", type = "error")
@@ -268,7 +270,7 @@ server <- function(input, output, session) {
   })
   
   
-  # Warning 2 - No from or to date
+  # Error 2 - No from date
   observe({
     if (!isTruthy(input$fromdate)){
       showNotification(id = "NoFromDate", "Please specify which date you want the portfolio optimization to start from", type = "error")
@@ -278,6 +280,7 @@ server <- function(input, output, session) {
     }
   })
   
+  # Error 3 - No to date
   observe({
     if (!isTruthy(input$todate)){
       showNotification(id = "NoToDate", "Please specify which date you want the portfolio optimization to start from", type = "error")
@@ -287,6 +290,14 @@ server <- function(input, output, session) {
     }
   })
   
+  observe({
+    if (length(input$manual) > 50){
+      showNotification("Loading may now take up to 1 minute", type = "warning")
+    }
+    else{
+      NULL
+    }
+  })
   
   
   # If sinstocks are unwanted -> make a subset of the dataframe without sinstocks
@@ -297,14 +308,8 @@ server <- function(input, output, session) {
       return(stocks_with_industry[stocks_with_industry$Ethics %notin% sinstocks,])
     }
     else{
-      if(isTRUE(input$selectionType == "Select unfit industries")){
-        return(stocks_with_industry[stocks_with_industry$Industry %notin% input$industry,])
+        return(stocks_with_industry)
       }
-      else{
-        return(stocks_with_industry[stocks_with_industry$Industry %in% input$industrySelect,])
-      }
-      
-    }
   })
   
   # Incorporate the sinstock subset with the green.stock only choice 
@@ -374,7 +379,7 @@ server <- function(input, output, session) {
   
   
   observeEvent(input$random, {  # Get random portfolio which does not include chosen undesired industry - the user can select the amount of stocks in the random portfolio
-    if (isTRUE(input$n_unique_stocks <= length(choice1()))){
+    if (isTRUE(input$n_unique_stocks <= length(choice1()))){ # Check if amount of random stocks to draw is less or equal than the available stocks
       updateSelectInput(session, "manual",
                         selected = sample(choice1(), input$n_unique_stocks))
     }
@@ -384,6 +389,32 @@ server <- function(input, output, session) {
     
   })
   
+  # -- Add warning when stock ratio input value is illigal
+  observe({
+    if (!isTruthy(input$stockmax)){
+      showNotification("Please insert a number in max stock ratio", type = "error")
+    }
+    else if (isTRUE(input$stockmax > 1)){
+    showNotification("Please insert a max stock ratio equal or lower than 1", type = "error")
+  
+    }
+    else{
+      NULL
+    }
+  })
+  
+  observe({
+    if (!isTruthy(input$stockmin)){
+      showNotification("Please insert a number in stock min ratio", type = "error")
+    }
+    
+    else if (isTRUE(input$stockmin < -1)){
+      showNotification("Please insert a stock min ratio equal or higher than -1", type = "error")
+    }
+    else{
+      NULL
+    }
+  })
   
   
   # Update min value in numeric input based on the amount of stocks you select to the portfolio
@@ -435,6 +466,19 @@ server <- function(input, output, session) {
   }, ignoreNULL = FALSE)
   
   
+  
+  
+  # Give user a message if there has been selected few stocks
+  observeEvent(input$update, {
+    if(length(input$manual) < 5){
+      showNotification("You may get an error, if so then select more stocks or other stocks", type = "message")
+    }
+    else{
+      NULL
+    }
+  })
+  
+  
   # -- If input is correct -> Go to next page when a confirming button is pressed
   
   # -- If input is not valid -> Make the user correct input values before sending the user to the next page
@@ -450,7 +494,10 @@ server <- function(input, output, session) {
       showNotification("Please write a valid date", type = "error")
     }
     else if(!isTruthy(input$manual)){
-      showNotification("Please choose at least 1 stock", type = "error")
+      showNotification("Please choose at least 2 stocks", type = "error")
+    }
+    else if(length(input$manual) < 2){
+      showNotification("Please select at least 2 but prefferably 5 or more stocks", type = "error")
     }
     else{
       updateTabsetPanel(session, "tabset1", 
@@ -459,23 +506,55 @@ server <- function(input, output, session) {
   
   })
   
-  # -- When confirm is clicked - send user to the results page
+  # -- If input is correct -> Go to next page when a confirming button is pressed
+  
+  # -- If input is not valid -> Make the user correct input values before sending the user to the next page
   observeEvent(input$update2, {
-    updateTabsetPanel(session, "tabset1",
-                      selected = "resultspanel")
+    if (!isTruthy(input$stockmax)){
+      showNotification("Please insert a number in max stock ratio", type = "error")
+    }
+    else if (isTRUE(input$stockmax > 1)){
+      showNotification("Please insert a max stock ratio equal or lower than 1", type = "error")
+    }
+    else if (isTRUE(input$stockmax < 1/length(input$manual))){
+      showNotification("Please select a higher stock max value", type = "error")
+    }
+    else if (!isTruthy(input$stockmin)){
+      showNotification("Please insert a number in stock min ratio", type = "error")
+    }
+    
+    else if (isTRUE(input$stockmin < -1)){
+      showNotification("Please insert a stock min ratio equal or higher than -1", type = "error")
+    }
+    else{
+      updateTabsetPanel(session, "tabset1",
+                        selected = "resultspanel")
+    }
+    
+   
   })
   
   
   
-  # -- Making a general function for user inputs to be applied to stock_input
+  # -- Making a general function for user inputs to be applied to stock_input in our imported Ban400-Functions script
   
   dataInput <- eventReactive(c(input$update, input$update2), {
     input_function(tickers1(), input$fromdate, input$todate)
   }, ignoreNULL = FALSE)
   
   
+  # dataInput 1 = tickers
+  # dataInput 2 = stock_prices
+  # dataInput 3 = returns_matrix
+  # dataInput 4 = stock_correlation
+  # dataInput 5 = stock_return with date
+  # dataInput 6 = stock_covariance
+  # dataInput 7 = portfolio weigths
+  # dataInput 8 = errors
+  # dataInput 9 = stock info
   
-  # -- Making another input function to be able to use subsets from the stock_opt_vol and stock_opt_sharpe functions.
+  
+  # -- Making input functions to be able to use subsets from the stock_opt_vol, stock_opt sortino and stock_opt_sharpe functions.
   
   vol_output <- eventReactive(c(input$update, input$update2), {
     stock_opt_vol(dataInput()[[1]], dataInput()[[7]], dataInput()[[3]], dataInput()[[6]], as.numeric(input$stockmax), as.numeric(input$stockmin))
@@ -504,11 +583,14 @@ server <- function(input, output, session) {
   
   # Before generating output for stats based upon chosen method - check that there is input
   # If there is no input -> display a error message where the user is informed about what the missing input is
+  # The validation is done for all the outputs to present a coherent message when something is going wrong
   
   output$stats <- renderTable({
+    nstocks <- length(input$manual)
     validate(
       need(isTruthy(input$rfrate), "Please input risk free rate at the stock selection page"),
       need(isTruthy(input$manual), "Please input stocks at the stock selection page"),
+      need(nstocks >= 2, "Please select more stocks"),
       need(isTruthy(input$fromdate), "Please input a valid date at the stock selection page"),
       need(isTruthy(input$todate), "Please input a valid date at the stock selection page")
     )
@@ -527,9 +609,11 @@ server <- function(input, output, session) {
   # Generate output for optimal volume based upon chosen method
   
   output$volstats <- renderDataTable({
+    nstocks <- length(input$manual)
     validate(
       need(isTruthy(input$rfrate), "Please input risk free rate at the stock selection page"),
       need(isTruthy(input$manual), "Please input stocks at the stock selection page"),
+      need(nstocks >= 2, "Please select more stocks"),
       need(isTruthy(input$fromdate), "Please input a valid date at the stock selection page"),
       need(isTruthy(input$todate), "Please input a valid date at the stock selection page")
     )
@@ -548,9 +632,11 @@ server <- function(input, output, session) {
   
   # Generate output for portfolio industry percentages
   output$vpiechart <- renderPlot({
+    nstocks <- length(input$manual)
     validate(
       need(isTruthy(input$rfrate), "Please input risk free rate at the stock selection page"),
       need(isTruthy(input$manual), "Please input stocks at the stock selection page"),
+      need(nstocks >= 2, "Please select more stocks"),
       need(isTruthy(input$fromdate), "Please input a valid date at the stock selection page"),
       need(isTruthy(input$todate), "Please input a valid date at the stock selection page")
     )
@@ -570,9 +656,11 @@ server <- function(input, output, session) {
   
   # Generate output for the correlation plot
   output$vcorr_plot <- renderPlot({
+    nstocks <- length(input$manual)
     validate(
       need(isTruthy(input$rfrate), "Please input risk free rate at the stock selection page"),
       need(isTruthy(input$manual), "Please input stocks at the stock selection page"),
+      need(nstocks >= 2, "Please select more stocks"),
       need(isTruthy(input$fromdate), "Please input a valid date at the stock selection page"),
       need(isTruthy(input$todate), "Please input a valid date at the stock selection page")
     )
@@ -595,9 +683,11 @@ server <- function(input, output, session) {
   
   # Generate output for the returns histogram
   output$vreturns_hist <- renderPlot({
+    nstocks <- length(input$manual)
     validate(
       need(isTruthy(input$rfrate), "Please input risk free rate at the stock selection page"),
       need(isTruthy(input$manual), "Please input stocks at the stock selection page"),
+      need(nstocks >= 2, "Please select more stocks"),
       need(isTruthy(input$fromdate), "Please input a valid date at the stock selection page"),
       need(isTruthy(input$todate), "Please input a valid date at the stock selection page")
     )
@@ -617,9 +707,11 @@ server <- function(input, output, session) {
   
   # Genereate output for the stock price history
   output$vstock_price_history <- renderPlot({
+    nstocks <- length(input$manual)
     validate(
       need(isTruthy(input$rfrate), "Please input risk free rate at the stock selection page"),
       need(isTruthy(input$manual), "Please input stocks at the stock selection page"),
+      need(nstocks >= 2, "Please select more stocks"),
       need(isTruthy(input$fromdate), "Please input a valid date at the stock selection page"),
       need(isTruthy(input$todate), "Please input a valid date at the stock selection page")
     )
@@ -638,9 +730,11 @@ server <- function(input, output, session) {
   
   # Generate output for the efficiency frontier
   output$vefficency_frontier <- renderPlot({
+    nstocks <- length(input$manual)
     validate(
       need(isTruthy(input$rfrate), "Please input risk free rate at the stock selection page"),
       need(isTruthy(input$manual), "Please input stocks at the stock selection page"),
+      need(nstocks >= 2, "Please select more stocks"),
       need(isTruthy(input$fromdate), "Please input a valid date at the stock selection page"),
       need(isTruthy(input$todate), "Please input a valid date at the stock selection page")
     )
@@ -649,9 +743,11 @@ server <- function(input, output, session) {
   
   # Generate output for the S&P500 comparison
   output$vcompare_SP500 <- renderPlot({
+    nstocks <- length(input$manual)
     validate(
       need(isTruthy(input$rfrate), "Please input risk free rate at the stock selection page"),
       need(isTruthy(input$manual), "Please input stocks at the stock selection page"),
+      need(nstocks >= 2, "Please select more stocks"),
       need(isTruthy(input$fromdate), "Please input a valid date at the stock selection page"),
       need(isTruthy(input$todate), "Please input a valid date at the stock selection page")
     )
@@ -670,9 +766,11 @@ server <- function(input, output, session) {
   
   # Generate output for the portfolio returns histogram
   output$vport_hist <- renderPlot({
+    nstocks <- length(input$manual)
     validate(
       need(isTruthy(input$rfrate), "Please input risk free rate at the stock selection page"),
       need(isTruthy(input$manual), "Please input stocks at the stock selection page"),
+      need(nstocks >= 2, "Please select more stocks"),
       need(isTruthy(input$fromdate), "Please input a valid date at the stock selection page"),
       need(isTruthy(input$todate), "Please input a valid date at the stock selection page")
     )
